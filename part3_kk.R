1c4b2a489c4621edeb3fd2aa3bd3a7ad001aba7a
kkopecky711 <kopeckykai89@gmail.com>
2019-10-21 13:56:55 -0700

    added infant mortality graph

--- part3_kk.R
+++ part3_kk.R
@@ -11,4 +11,8 @@ db <- read_csv(here("data", "disease_burden.csv")) %>%
   clean_names() %>% 
   rename(deaths_per_100k = death_rate_per_100_000)
 
+# Create a subset, then graph:
+db_sub <- db %>% 
+  filter(country_name %in% c("United States", "Japan", "Afghanistan", "Somalia"))
+
 
