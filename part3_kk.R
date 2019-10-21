b9ab8a0b578ea66a4b3d076f94da9a066f927612
kkopecky711 <kopeckykai89@gmail.com>
2019-10-21 13:47:20 -0700

    Cleaning disease burden df

---
+++ part3_kk.R
@@ -0,0 +1,14 @@
+# ---------------
+# Kai Kopecky
+# --------------
+
+library(tidyverse)
+library(janitor)
+library(here)
+
+# Read in data:
+db <- read_csv(here("data", "disease_burden.csv")) %>% 
+  clean_names() %>% 
+  rename(deaths_per_100k = death_rate_per_100_000)
+
+
